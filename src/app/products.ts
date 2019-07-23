8d88645d04b88745e465a57d747ca26cefa9ca9e
Dezhuang <dezhuang@gmail.com>
2019-07-22 22:28:03 -0400

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,16 +1,16 @@
 export const products = [
   {
-    name: 'Beijing Tiananmen Square improvement',
+    name: '北京天安门改造工程 Beijing Tiananmen Square Improvement',
     price: "1000000000",
     description: 'Modernize the world largest square'
   },
   {
-    name: 'Qingdao Huangdao Subway II',
+    name: '天津地铁二期 Tianjin Subway II',
     price: "2000000",
     description: 'Extend the subway to every street'
   },
   {
-    name: 'Shanghai Municipal Building',
+    name: '上海政府大厦扩建 Shanghai Municipal Building Extension',
     price: "300000",
     description: 'Redesign and buid the goverment building'
   }
