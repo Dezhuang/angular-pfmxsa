1443917c0fe1e0776387364be05b4ad0e7993afe
Dezhuang <dezhuang@gmail.com>
2019-07-22 21:47:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pfmxsa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pfmxsa)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Beijing Tiananmen Square',
+    price: 799,
+    description: 'Modernize the world largest square'
+  },
+  {
+    name: 'Qingdao Huangdao Subway II',
+    price: 699,
+    description: 'Extend the subway to every street'
+  },
+  {
+    name: 'Shanghai Municipal Building',
+    price: 299,
+    description: 'Redesign and buid the goverment building'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
